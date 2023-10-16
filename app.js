e3cfd36a3abea6050aee94e3bb91ff9eb13386a3
salwa mazarin <tech_dev4@indicocompany.com>
2023-10-16 20:42:37 +0700

    update cors

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ const cors = require('cors');
 app.use(express.json());
 app.use(cors());
 
-app.use(cors({
-    origin: 'http://localhost:3000',
-    methods: 'GET,POST,PUT,DELETE',
-    allowedHeaders: 'Content-Type,Authorization',
-}));
+// app.use(cors({
+//     origin: 'http://localhost:3000',
+//     methods: 'GET,POST,PUT,DELETE',
+//     allowedHeaders: 'Content-Type,Authorization',
+// }));
 app.use(cors({
     origin: 'https://todo-list-fe-murex.vercel.app/',
     methods: 'GET,POST,PUT,DELETE',
